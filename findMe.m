a25168c0fe208c679210b5c91bcf24156c78256f
smcclain56 <43018457+smcclain56@users.noreply.github.com>
2019-04-04 16:41:39 -0700

    Update findMe.m

--- findMe.m
+++ findMe.m
@@ -1,19 +1,25 @@
 function res = findMe(A,b,x,v)
+    %get column and row size of A
     [m,n] = size(A);
     
     %Determine how many problem constraints we have 
+    %potential problems: if a constraint must be greater than or equal to a positive number, 
+    %                    if a constraint must be less than or equal to a negative number,
+    %                    
     pardner = zeros(1,m);
+    %Identify potential problems for each row
     for i = 1:m
         isNeg = 0;
-        for j = 1:n
+        for j = 1:n %parse through columns
             if A(i,j) == 0
-                
-            elseif A(i,j) < 0
-                isNeg = 1;
-            elseif A(i,j) > 0
+                %do nothing 
+            elseif A(i,j) < 0 %set isNeg
+                isNeg = 1; 
+            elseif A(i,j) > 0 %set isNeg
                 isNeg = 0;
             end
         end
+        %update pardner(i) for each row i
         if isNeg && b(i) > 0
            pardner(i) = 1;
         elseif ~isNeg && b(i) < 0
@@ -24,14 +30,20 @@ function res = findMe(A,b,x,v)
     
     %add new vars to A, x, and c
     if ~isempty(find(~pardner))
+        %if find(~(pardner)) is not full of zeros we need to find a new initial solution
+        
         %disp(find(~pardner))
+        %create a newX vector with a new row for every problem detected
         num = length(pardner) - length(find(~pardner));
         start = length(x);
+        %have newX contain a 1 in the new rows added and a 0 otherwise
         newX = zeros(1,start + num);
         for i = 1:start + num
             newX(i) = i;
         end
         %disp(newX)
+        
+        %create a newC vector of same length as newX
         newC = zeros(1,length(newX));
         q = num;
         tail = length(newX) + 1;
@@ -39,18 +51,23 @@ function res = findMe(A,b,x,v)
             newC(tail - q) = 1;
             q = q - 1;
         end
-        %disp(newC)
+        % disp(newC)
         
-        %Make new A
-        %disp(A)
+        % Make new A matrix
+        % disp(A)
+        % Add the new problem columns to the size of newA
         newA = zeros(m, n + num);
         here = num - 1;
+        % traverse through A rows
         for i = 1:m
             foundOne = 0;
+            % traverse through A columns
             for j = 1:n + num
                 if j <= n
+                    % WHAT IS HAPPENING HERE
                     newA(i,j) = A(i,j);
                 else
+                    %Otherwise we DO WHAT?
                     if pardner(i) == 1 && j == (n + num) - here && ~foundOne
                         newA(i,j) = 1;
                         here = here - 1;
@@ -60,13 +77,15 @@ function res = findMe(A,b,x,v)
             end
         end
         
-        %disp(newA)
+        % disp(newA)
         
-        %Make new x_not
+        % Make new x_not
         x_not = zeros(length(newX),1);
+        % WHAT DOES FIND DO AGAIN?
         aInds = find(newC);
         sInds = find(~newC);
         at = 1;
+        % NEED A REMINDER ON THIS
         for i = 1:length(pardner)
            if pardner(i) == 1
                x_not(aInds(at)) = b(i);
@@ -80,10 +99,13 @@ function res = findMe(A,b,x,v)
         newB = find(x_not)';
         newN = find(~x_not)';
         
+        % Call Maximize() on new data found
+        % REMINDER ON THIS AS WELL
         hold = Maximize(x_not, newB, newN, -newC, -newA, newX, -b);
         diff = length(newX) - length(x);
         res = hold(1:length(hold)-diff, 1);
-    else
+    else 
+        %if no problems were found we return -1
         res = -1;
     end
 end
